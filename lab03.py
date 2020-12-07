643b8707c6668def8a2b850af939ccb04835f2b5
Guanming Chen <58407616+cgm629@users.noreply.github.com>
2020-12-07 19:16:32 +0800

    Add files via upload

---
+++ lab03.py
@@ -0,0 +1,45 @@
+""" Optional problems for Lab 3 """
+
+def is_prime(n):
+    """Returns True if n is a prime number and False otherwise.
+    >>> is_prime(2)
+    True
+    >>> is_prime(16)
+    False
+    >>> is_prime(521)
+    True
+    """
+    if n == 1:
+        return False  # 1 is not considered to be a prime number
+    helper = lambda i: True if i == n else False if n % i == 0 else helper(i + 1)
+    return helper(2)
+
+def gcd(a, b):
+    """Returns the greatest common divisor of a and b.
+    Should be implemented using recursion.
+    >>> gcd(34, 19)
+    1
+    >>> gcd(39, 91)
+    13
+    >>> gcd(20, 30)
+    10
+    >>> gcd(40, 40)
+    40
+    """
+    min_val, max_val = min(a, b), max(a, b)
+    return min_val if max_val % min_val == 0 else gcd(min_val, max_val % min_val)
+
+def ten_pairs(n):
+    """Return the number of ten-pairs within positive integer n.
+    >>> ten_pairs(7823952)
+    3
+    >>> ten_pairs(55055)
+    6
+    >>> ten_pairs(9641469)
+    6
+    """
+    def count_targets(n, tar, acc):
+        return acc if n == 0 else count_targets(n // 10, tar, acc + 1 if n % 10 == tar else acc)
+    def iterate_digits(n, acc):
+        return acc if n == 0 else iterate_digits(n // 10, acc + count_targets(n // 10, 10 - n % 10, 0))
+    return iterate_digits(n, 0)
---
+++ lab03_extra.py
@@ -0,0 +1,62 @@
+""" Lab 3: Recursion and Python Lists """
+
+def skip_add(n):
+    """ Takes a number x and returns x + x-2 + x-4 + x-6 + ... + 0.
+    >>> skip_add(5)  # 5 + 3 + 1 + 0
+    9
+    >>> skip_add(10) # 10 + 8 + 6 + 4 + 2 + 0
+    30
+    >>> # Do not use while/for loops!
+    >>> from construct_check import check
+    >>> check('lab03.py', 'skip_add',
+    ...       ['While', 'For'])
+    True
+    """
+    helper = lambda n, acc: acc if n < 0 else helper(n - 2, acc + n)
+    return helper(n, 0)
+
+
+this_file = __file__
+
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, and return the
+    number of elements in the sequence.
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    >>> # Do not use while/for loops!
+    >>> from construct_check import check
+    >>> check(this_file, 'hailstone',
+    ...       ['While', 'For'])
+    True
+    """
+    gen_next_value = lambda x: x // 2 if x % 2 == 0 else x * 3 + 1
+    helper = lambda n, acc: print(n) or (acc + 1 if n == 1 else helper(gen_next_value(n), acc + 1))
+    return helper(n, 0)
+
+def summation(n, term):
+
+    """Return the sum of the first n terms in the sequence defined by term.
+    Implement using recursion!
+    >>> summation(5, lambda x: x * x * x) # 1^3 + 2^3 + 3^3 + 4^3 + 5^3
+    225
+    >>> summation(9, lambda x: x + 1) # 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+    54
+    >>> summation(5, lambda x: 2**x) # 2^1 + 2^2 + 2^3 + 2^4 + 2^5
+    62
+    >>> # Do not use while/for loops!
+    >>> from construct_check import check
+    >>> check(this_file, 'summation',
+    ...       ['While', 'For'])
+    True
+    """
+    assert n >= 1
+    helper = lambda n, acc: acc if n == 0 else helper(n - 1, acc + term(n))
+    return helper(n, 0)
