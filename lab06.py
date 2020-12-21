519f2eeddc5298b88800966fc11a870eaff31d27
Guanming Chen <58407616+cgm629@users.noreply.github.com>
2020-12-21 16:44:59 +0800

    Create lab06.py

---
+++ lab06.py
@@ -0,0 +1,32 @@
+def make_adder_inc(n):
+    """
+    >>> adder1 = make_adder_inc(5)
+    >>> adder2 = make_adder_inc(6)
+    >>> adder1(2) 
+    7
+    >>> adder1(2) # 5 + 2 + 1
+    8
+    >>> adder1(10) # 5 + 10 + 2
+    17
+    >>> [adder1(x) for x in [1, 2, 3]]
+    [9, 11, 13]
+    >>> adder2(5)
+    11
+    """
+    base = n
+    def inner(i):
+        nonlocal base
+        res = base + i
+        base += 1
+        return res
+    return inner
+
+def map(fn, lst):
+    """Maps fn onto lst using mutation.
+    >>> original_list = [5, -1, 2, 0]
+    >>> map(lambda x: x * x, original_list)
+    >>> original_list
+    [25, 1, 4, 0]
+    """
+    for index, value in enumerate(lst):
+        lst[index] = fn(value)
