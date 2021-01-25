8121cc33834635926decfb4e935ac004d5c61acc
Guanming Chen <58407616+cgm629@users.noreply.github.com>
2021-01-25 19:53:25 +0800

    Create lab13_extra.py

---
+++ lab/lab13_extra.py
@@ -0,0 +1,20 @@
+.read lab13.sql
+
+CREATE TABLE su19favpets AS
+  SELECT pet, COUNT(*)
+  FROM students
+  GROUP BY pet
+  HAVING COUNT(*) > 1
+  ORDER BY COUNT(*) DESC
+  LIMIT 10;
+
+CREATE TABLE su19dog AS
+  SELECT pet, COUNT(*)
+  FROM students
+  WHERE pet = 'dog';
+
+CREATE TABLE obedienceimages AS
+  SELECT seven, instructor, COUNT(*)
+  FROM students
+  WHERE seven = '7'
+  GROUP BY instructor;
