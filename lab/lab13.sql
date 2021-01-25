5b5f4054a84d081e4de322f786046cafeb6e1588
Guanming Chen <58407616+cgm629@users.noreply.github.com>
2021-01-25 19:52:40 +0800

    Create lab13.sql

---
+++ lab/lab13.sql
@@ -0,0 +1,26 @@
+.read su19data.sql
+
+CREATE TABLE obedience AS
+  SELECT seven, instructor
+  FROM students;
+
+CREATE TABLE smallest_int AS
+  SELECT time, smallest
+  FROM students
+  WHERE smallest > 2
+  ORDER BY smallest
+  LIMIT 20;
+
+CREATE TABLE matchmaker AS
+  SELECT a.pet, a.song, a.color, b.color
+  FROM students as a, students as b
+  WHERE a.time < b.time
+    AND a.pet = b.pet
+    AND a.song = b.song;
+
+CREATE TABLE smallest_int_having AS
+  SELECT time, smallest
+  FROM students
+  GROUP BY smallest
+  HAVING COUNT(*) = 1
+  ORDER BY smallest;
